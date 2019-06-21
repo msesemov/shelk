28d5d9999222d3509c65c461175c9fa6c863501f
msesemov <msesemov@gmail.com>
2019-06-21 19:42:52 +0000

    task_6_3 done

--- task_6_2a.py
+++ task_6_2a.py
@@ -2,22 +2,20 @@
 
 ip = input('Enter IP-address: ')
 octets = ip.split('.')
+d_oct = []
+for octet in octets:
+	d_oct.append(int(octet))
 
-if ip.count('.') == 3:
-	for octet in octets:
-		if octet >=0 and octet <= 255:
-
-
-
-			if int(octets[0]) >= 1 and int(octets[0]) <= 223:
-				print('unicast')
-			elif int(octets[0]) >= 224 and int(octets[0]) <= 239:
-				print('multicast')
-			elif int(octets[0]) == 255 and int(octets[1]) == 255 and int(octets[2]) == 255 and int(octets[3]) == 255:
-				print('local broadcast')
-			elif int(octets[0]) == 0 and int(octets[1]) == 0 and int(octets[2]) == 0 and int(octets[3]) == 0:
-				print('unassigned')
-			else:
-				print('unused')
+if ip.count('.') == 3 and d_oct[0] >=0 and d_oct[0] <= 255 and d_oct[1] >=0 and d_oct[1] <= 255 and d_oct[2] >=0 and d_oct[2] <= 255 and d_oct[3] >=0 and d_oct[3] <= 255:
+	if d_oct[0] >= 1 and d_oct[0] <= 223:
+		print('unicast')
+	elif d_oct[0] >= 224 and d_oct[0] <= 239:
+		print('multicast')
+	elif d_oct[0] == 255 and d_oct[1] == 255 and d_oct[2] == 255 and d_oct[3] == 255:
+		print('local broadcast')
+	elif d_oct[0] == 0 and d_oct[1] == 0 and d_oct[2] == 0 and d_oct[3] == 0:
+		print('unassigned')
+	else:
+		print('unused')
 else:
 	print('incorrect IP-address')
