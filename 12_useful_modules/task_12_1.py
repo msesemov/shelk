d272e30720d757e653e7e67c09181091fb112c42
msesemov <msesemov@gmail.com>
2019-07-06 23:51:29 +0000

    task_12_1 done

---
+++ 12_useful_modules/task_12_1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Задание 12.1
+
+Создать функцию ping_ip_addresses, которая проверяет доступность IP-адресов.
+
+Функция ожидает как аргумент список IP-адресов.
+
+Функция должна возвращать кортеж с двумя списками:
+* список доступных IP-адресов
+* список недоступных IP-адресов
+
+Для проверки доступности IP-адреса, используйте ping.
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+'''
+
+import subprocess
+
+
+def ping_ip_addresses(ip_list):
+    alive = []
+    unreachable = []
+    for ip in ip_list:
+        reply = subprocess.run(['ping', '-c', '3', '-n', ip],
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE,
+                                    encoding='utf-8')
+        if reply.returncode == 0:
+            alive.append(ip)
+        else:
+            unreachable.append(ip)
+    result = (alive, unreachable)
+    return result
+
+
+if __name__ == '__main__':
+    ip_list = ['8.8.8.8', '4.4.4.4', '1.1.1.1', '123.324.41.12', '123.2.3.3']
+    print(ping_ip_addresses(ip_list))
