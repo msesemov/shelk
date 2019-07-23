0189171cdc025539e3e2d32e9807dc252807dd82
msesemov <msesemov@gmail.com>
2019-07-23 17:36:28 +0300

    task_18_1 done

--- 18_db/task_18_1/create_db.py
+++ 18_db/task_18_1/create_db.py
@@ -4,19 +4,24 @@
 import os
 import sqlite3
 
-data_filename = 'dhcp_snooping.txt'
-db_filename = 'dhcp_snooping.db'
-schema_filename = 'dhcp_snooping_schema.sql'
 
-db_exists = os.path.exists(db_filename)
+def create_db(db_filename):
+    db_exists = os.path.exists(db_filename)
 
-conn = sqlite3.connect(db_filename)
+    conn = sqlite3.connect(db_filename)
 
-if not db_exists:
-    print('Создаю базу данных...')
-    with open(schema_filename, 'r') as f:
-        schema = f.read()
-    conn.executescript(schema)
-    print('Done')
-else:
-    print('База данных существует')
+    if not db_exists:
+        print('Создаю базу данных...')
+        with open(schema_filename, 'r') as f:
+            schema = f.read()
+        conn.executescript(schema)
+        print('Done')
+    else:
+        print('База данных существует')
+
+
+if __name__ == '__main__':
+    data_filename = 'dhcp_snooping.txt'
+    db_filename = 'dhcp_snooping.db'
+    schema_filename = 'dhcp_snooping_schema.sql'
+    create_db(db_filename)
