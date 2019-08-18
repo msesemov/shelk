52b08889f076c34a4f1488e1cdd306a10f0c5049
msesemov <msesemov@gmail.com>
2019-08-19 01:27:33 +0300

    task_21_5 done

--- 21_jinja2/task_21_5.py
+++ 21_jinja2/task_21_5.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
 Задание 21.5
@@ -14,22 +15,38 @@ cisco_vpn_1.txt и cisco_vpn_2.txt.
 
 Создать функцию create_vpn_config, которая использует эти шаблоны для генерации конфигурации VPN на основе данных в словаре data.
 
-Параметры функции:
-* template1 - имя файла с шаблоном, который создает конфигурацию для одной строны туннеля
-* template2 - имя файла с шаблоном, который создает конфигурацию для второй строны туннеля
-* data_dict - словарь со значениями, которые надо подставить в шаблоны
+
 
 Функция должна возвращать кортеж с двумя конфигурациямя (строки), которые получены на основе шаблонов.
 
 Примеры конфигураций VPN, которые должна возвращать функция create_vpn_config в файлах
 cisco_vpn_1.txt и cisco_vpn_2.txt.
 '''
+from jinja2 import Environment, FileSystemLoader
 
-data = {
-    'tun_num': 10,
-    'wan_ip_1': '192.168.100.1',
-    'wan_ip_2': '192.168.100.2',
-    'tun_ip_1': '10.0.1.1 255.255.255.252',
-    'tun_ip_2': '10.0.1.2 255.255.255.252'
-}
 
+def create_vpn_config(template1 , template2, data_dict):
+    '''
+    Параметры функции:
+    * template1 - имя файла с шаблоном, который создает конфигурацию для одной строны туннеля
+    * template2 - имя файла с шаблоном, который создает конфигурацию для второй строны туннеля
+    * data_dict - словарь со значениями, которые надо подставить в шаблоны
+    '''
+
+    env = Environment(loader=FileSystemLoader('templates'),
+                        trim_blocks=True, lstrip_blocks=True)
+    template1 = env.get_template(template1)
+    template2 = env.get_template(template2)
+
+    return (template1.render(data_dict), template2.render(data_dict))
+
+
+if __name__ == '__main__':
+    data = {
+        'tun_num': 10,
+        'wan_ip_1': '192.168.100.1',
+        'wan_ip_2': '192.168.100.2',
+        'tun_ip_1': '10.0.1.1 255.255.255.252',
+        'tun_ip_2': '10.0.1.2 255.255.255.252'
+    }
+    print(create_vpn_config('gre_ipsec_vpn_1.txt' , 'gre_ipsec_vpn_2.txt', data))
--- 21_jinja2/task_21_5a.py
+++ 21_jinja2/task_21_5a.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
 Задание 21.5a
@@ -27,11 +28,106 @@
 Для этого задания нет теста!
 '''
 
-data = {
-    'tun_num': None,
-    'wan_ip_1': '192.168.100.1',
-    'wan_ip_2': '192.168.100.2',
-    'tun_ip_1': '10.0.1.1 255.255.255.252',
-    'tun_ip_2': '10.0.1.2 255.255.255.252'
-}
+from jinja2 import Environment, FileSystemLoader
+import netmiko
+import paramiko
+import re
 
+
+def create_vpn_config(template1 , template2, data_dict):
+    '''
+    Параметры функции:
+    * template1 - имя файла с шаблоном, который создает конфигурацию для одной строны туннеля
+    * template2 - имя файла с шаблоном, который создает конфигурацию для второй строны туннеля
+    * data_dict - словарь со значениями, которые надо подставить в шаблоны
+    '''
+
+    env = Environment(loader=FileSystemLoader('templates'),
+                        trim_blocks=True, lstrip_blocks=True)
+    template1 = env.get_template(template1)
+    template2 = env.get_template(template2)
+
+    return (template1.render(data_dict), template2.render(data_dict))
+
+
+def send_show_command(device, command):
+    try:
+        with netmiko.ConnectHandler(**device) as ssh:
+            ssh.enable()
+
+            result = ssh.send_command(command)
+            return result
+    except paramiko.ssh_exception.AuthenticationException as e:
+        return e
+    except netmiko.ssh_exception.NetMikoTimeoutException as e:
+        return e
+
+
+def send_config_commands(device, config_commands, verbose=False):
+    template = '''Команда "{}" выполнилась с ошибкой "{}" на устройстве {}'''
+    rex_err = r'#(.+)\n\s*\^?\n?%\s(\w+\s\w+\s?\w+)'
+    rex_ok = r'#(.+)\n\w'
+
+    if verbose:
+        print('Connection to device {}'.format(device['ip']))
+
+    try:
+        with netmiko.ConnectHandler(**device) as ssh:
+            ssh.enable()
+            result = ssh.send_config_set(config_commands)
+            match = re.finditer(rex_err, result)
+            bad = {}
+            good = {}
+            if match:
+                for m in match:
+                    print(template.format(*m.groups(), device['ip']))
+                    if_in = input('Продолжать выполнять команды? [y]/n:')
+                    if if_in == 'n':
+                        bad.update({m.groups()[0]: m.groups()[1]})
+                        break
+                    elif if_in == 'y' or not if_in:
+                        bad.update({m.groups()[0]: m.groups()[1]})
+            match = re.finditer(rex_ok, result)
+            if match:
+                for m in match:
+                    if m.groups()[0] != 'end':
+                        good.update({m.groups()[0]: None})
+            return (good, bad)
+    except paramiko.ssh_exception.AuthenticationException as e:
+        return e
+    except netmiko.ssh_exception.NetMikoTimeoutException as e:
+        return e
+
+
+def configure_vpn(src_device_params, dst_device_params, src_template, dst_template, vpn_data_dict):
+    intf1 = send_show_command(src_device_params, 'show ip int brief')
+    intf2 = send_show_command(dst_device_params, 'show ip int brief')
+    regex = r'^(\w+)(\d+)\s+'
+    match1 = re.findall(regex, intf1)
+    if match1:
+        for m in match1:
+            print(m)
+    print(intf1, intf2)
+
+
+if __name__ == '__main__':
+
+    data = {
+        'tun_num': None,
+        'wan_ip_1': '192.168.100.1',
+        'wan_ip_2': '192.168.100.2',
+        'tun_ip_1': '10.0.1.1 255.255.255.252',
+        'tun_ip_2': '10.0.1.2 255.255.255.252'
+    }
+    src_dev = {'device_type': 'cisco_ios',
+                'ip': '192.168.100.1',
+                'username': 'cisco',
+                'password': 'cisco',
+                'secret': 'cisco'}
+    dst_dev = {'device_type': 'cisco_ios',
+                'ip': '192.168.100.2',
+                'username': 'cisco',
+                'password': 'cisco',
+                'secret': 'cisco'}
+
+    configure_vpn(src_dev, dst_dev, 'gre_ipsec_vpn_1.txt' , 'gre_ipsec_vpn_2.txt', data)
